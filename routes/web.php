42a0c9596f74cbc38b67059f3882208cde29c24b
Shatrov Denis <denydenyden1993@gmail.com>
2020-04-22 03:02:39 +0300

    0.0.2 [lesson-13]

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function (){
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+//Blog Admin
+$group_data = [
+    'namespace' =>  'Blog\Admin',
+    'prefix'    =>  'admin/blog'
+];
+Route::group($group_data, function (){
+    //BlogCategory
+    $methods = ['index', 'create', 'store', 'edit', 'update'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
